49478a9090e0e50eef3e09b589fd51f27bd96b32
Joel Rieke <joel_rieke@trimble.com>
2025-05-03 11:44:03 -0700

    Checking in for safekeeping

---
+++ flow/flow.go
@@ -0,0 +1,127 @@
+package flow
+
+import (
+	"database/sql"
+	"io"
+	"log"
+	"sync"
+
+	sqle "github.com/dolthub/go-mysql-server/sql"
+	tccore "github.com/trimble-oss/tierceron-core/v2/core"
+)
+
+type FlowType int64
+type FlowNameType string
+
+func (fnt FlowNameType) TableName() string {
+	return string(fnt)
+}
+
+type PermissionUpdate interface{}
+type FlowStateUpdate interface {
+	// NewFlowState() CurrentFlowState
+}
+type CurrentFlowState interface{}
+type TemplateData interface{}
+type FlowContext interface {
+	IsInit() bool
+	SetInit(bool)
+	IsRestart() bool
+	SetRestart(bool)
+	CancelTheContext() bool
+	FlowLocker()
+	FlowUnlocker()
+	FlowSyncModeMatchAny([]string) bool
+	FlowSyncModeMatch(string, bool) bool
+	GetFlowSyncMode() string
+	SetFlowSyncMode(string)
+	GetFlowSourceAlias() string
+	SetFlowSourceAlias(string)
+	SetChangeFlowName(string)
+	GetFlowStateState() int64
+	GetFlowState() CurrentFlowState
+	SetFlowState(CurrentFlowState)
+	GetPreviousFlowState() CurrentFlowState
+	SetPreviousFlowState(CurrentFlowState)
+	SetFlowData(TemplateData)
+	HasFlowSyncFilters() bool
+	GetFlowStateSyncFilterRaw() string
+	GetFlowSyncFilters() []string
+	GetFlowName() string
+	NewFlowStateUpdate(string, string) FlowStateUpdate
+	GetCurrentFlowStateUpdateByDataSource(string) chan CurrentFlowState
+	UpdateFlowStateByDataSource(string)
+	PushState(string, FlowStateUpdate)
+	GetUpdatePermission() PermissionUpdate
+	GetFlowUpdate(CurrentFlowState) FlowStateUpdate
+	GetRemoteDataSourceAttribute(string) interface{}
+	// tfContext.NewFlowStateUpdate(strconv.Itoa(int(previousState.State)), tfContext.GetPreviousFlowSyncMode())
+	GetLogger() *log.Logger
+}
+
+type FlowConfigContext interface {
+	ConfigTemplate(string, bool, string, string, bool, bool) (string, map[int]string, bool, error)
+	LogInfo(string)
+	LogErrorMessage(string, bool)
+	SetWantCerts(bool)
+}
+
+func SyncCheck(syncMode string) string {
+	switch syncMode {
+	case "nosync":
+		return " with no syncing"
+	case "push":
+		return " with push sync"
+	case "pull":
+		return " with pull sync"
+	case "pullonce":
+		return " to pull once"
+	case "pushonce":
+		return " to push once"
+	case "pullsynccomplete":
+		return " - Pull synccomplete..waiting for new syncMode value"
+	case "pullcomplete":
+		return " - Pull complete..waiting for new syncMode value"
+	case "pushcomplete":
+		return " - Push complete..waiting for new syncMode value"
+	case "pusherror":
+		return " - Push error..waiting for new syncMode value"
+	case "pullerror":
+		return " - Pull error..waiting for new syncMode value"
+	default:
+		return "...waiting for new syncMode value"
+	}
+}
+
+type FlowMachineContext interface {
+	GetEnv() string
+	GetDatabaseName() string
+	GetTableModifierLock() *sync.Mutex
+	TableCollationIdGen(string) sqle.CollationID
+	Init(map[string]map[string]interface{}, []string, []FlowNameType, []FlowNameType) error
+	AddTableSchema(sqle.PrimaryKeySchema, FlowContext)
+	CreateTableTriggers(FlowContext, string)
+	CreateTable(ctx *sqle.Context, name string, schema sqle.PrimaryKeySchema, collation sqle.CollationID) error
+	CreateCompositeTableTriggers(FlowContext, string, string, func(string, string, string, string) string, func(string, string, string, string) string, func(string, string, string, string) string)
+	CreateDataFlowTableTriggers(FlowContext, string, string, string, func(string, string, string, string, string) string, func(string, string, string, string, string) string, func(string, string, string, string, string) string)
+	GetFlowConfiguration(FlowContext, string) (map[string]interface{}, bool)
+	ProcessFlow(FlowConfigContext, FlowContext, func(FlowMachineContext, FlowContext) error, map[string]interface{}, map[string]map[string]interface{}, FlowNameType, FlowType) error
+	SetPermissionUpdate(FlowContext) // tfmContext.SetPermissionUpdate(tfContext)
+	//	seedVaultCycle(FlowContext, string, interface{}, func(interface{}, map[string]interface{}, interface{}, string, string, func(interface{}, map[string]interface{}) (string, []string, [][]interface{}, error)) (string, error), func(FlowContext, map[string]interface{}, map[string]interface{}, []string) error, bool)
+	//	seedTrcDbCycle(FlowContext, string, interface{}, func(interface{}, map[string]interface{}, interface{}, string, string, func(interface{}, map[string]interface{}) (string, []string, [][]interface{}, error)) (string, error), func(FlowContext, map[string]interface{}, map[string]interface{}, []string) error, bool, chan bool)
+	SyncTableCycle(FlowContext, string, interface{}, func(interface{}, map[string]interface{}, interface{}, string, string, func(interface{}, map[string]interface{}) (string, []string, [][]interface{}, error)) (string, error), func(FlowContext, map[string]interface{}, map[string]interface{}, []string) error, bool)
+	SelectFlowChannel(FlowContext) <-chan interface{}
+	GetCacheRefreshSqlConn(FlowContext, string, map[string]interface{}) (*sql.DB, error)
+	CallDBQuery(FlowContext, map[string]interface{}, map[string]sqle.Expression, bool, string, []FlowNameType, string) ([][]interface{}, bool)
+	GetDbConn(FlowContext, string, string, map[string]interface{}) (*sql.DB, error)
+	CallAPI(map[string]string, string, string, io.Reader, bool) (map[string]interface{}, int, error)
+	SetEncryptionSecret()
+	Log(string, error)
+	LogInfo(string)
+	GetLogger() *log.Logger
+	PathToTableRowHelper(FlowContext) ([]interface{}, error)
+	DeliverTheStatistic(FlowContext, *tccore.TTDINode, string, string, string, bool)
+	LoadBaseTemplate(FlowContext) (TemplateData, error) //var baseTableTemplate extract.TemplateResultData , tfContext.GoMod, tfContext.FlowSource, tfContext.Flow.ServiceName(), tfContext.FlowPath
+
+	//	writeToTableHelper(FlowContext, map[string]string, map[string]string) []interface{}
+}

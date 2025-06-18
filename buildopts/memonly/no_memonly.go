c6470f9624b64b4cf9e60887765f04929e8a3117
Joel Rieke <joel_rieke@trimble.com>
2025-06-18 12:05:29 -0700

    Moving coreconfig, trcshfs, and some buildopts to this layer

---
+++ buildopts/memonly/no_memonly.go
@@ -0,0 +1,9 @@
+//go:build !memonly
+// +build !memonly
+
+package memonly
+
+// IsMemonly returns true if memonly build flag is specified, false otherwise.
+func IsMemonly() bool {
+	return false
+}

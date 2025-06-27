f2c5326172857f12fad1d80d781a0a3757d0b073
Meghan Bailey <meghan_bailey@trimble.com>
2025-06-27 10:38:59 -0700

    Add initial version of bitmask

--- bitlock/bitlock.go
+++ bitlock/bitlock.go
@@ -1,1 +1,76 @@
 package bitlock
+
+import (
+	"sync"
+	"sync/atomic"
+)
+
+var bitmask *atomic.Uint64 = new(atomic.Uint64)
+
+var bitmaskLock *sync.RWMutex
+
+var bitmaskUpdateChan chan bool
+
+var listening = false
+
+func InitBitMask(size int) {
+	if size <= 0 {
+		return
+	}
+	bitmask.Store(0)
+	bitmaskLock = &sync.RWMutex{}
+	bitmaskUpdateChan = make(chan bool)
+}
+
+func Lock(flowID uint64) {
+	if bitmask == nil || bitmaskLock == nil {
+		return
+	}
+
+check:
+	conflict := checkConflict(flowID)
+
+	if conflict {
+		bitmaskLock.Lock()
+		listening = true
+		bitmaskLock.Unlock()
+		<-bitmaskUpdateChan
+		goto check
+	}
+
+	bitmaskLock.Lock()
+	currentVal := bitmask.Load()
+	result := currentVal ^ flowID
+	bitmask.Store(result)
+	if listening {
+		bitmaskUpdateChan <- true
+		listening = false
+	}
+	bitmaskLock.Unlock()
+}
+
+func Unlock(flowID uint64) {
+	if bitmask == nil || bitmaskLock == nil {
+		return
+	}
+
+	bitmaskLock.Lock()
+	currentVal := bitmask.Load()
+	result := currentVal ^ flowID
+	bitmask.Store(result)
+	if listening {
+		bitmaskUpdateChan <- true
+		listening = false
+	}
+	bitmaskLock.Unlock()
+}
+
+func checkConflict(flowID uint64) bool {
+	if bitmask == nil || bitmaskLock == nil {
+		return false
+	}
+	bitmaskLock.RLock()
+	defer bitmaskLock.RUnlock()
+	currentMask := bitmask.Load()
+	return (currentMask & flowID) != 0
+}
